568d8908c783ad59ec86f9640ce416c99ebe5506
PINKU9627 <142759419+PINKU9627@users.noreply.github.com>
2023-11-03 12:29:45 +0530

    Update test.js

--- test.js
+++ test.js
@@ -31,7 +31,7 @@ describe("Test2", () => {
 	console.log( "executes before every test" );
 	});
 	
-	it("Is returning 4 when adding 2 + 3", () => {
+	it("Is returning 5 when adding 2 + 3", () => {
 	assert.equal(2 + 3, 5);
 	});
 
